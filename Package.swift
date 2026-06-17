aa21634ad5dbd496d8fbc2261fcf302ce6b08d0f
Ovi Trif <ovitrif@proton.me>
2026-06-17 18:19:49 +0200

    fix: release paykit rc20

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let tag = "v0.1.0-rc19"
-let checksum = "edf888da514f26d77d08acdcaa7e13b693251078100a1232e9a41c08c53e77a8"
+let tag = "v0.1.0-rc20"
+let checksum = "1bfbc83ab4e66540489902ab48cc1abcb00b4cc34492f88552f36402e333fded"
 let url = "https://github.com/pubky/paykit-rs/releases/download/\(tag)/Paykit.xcframework.zip"
 
 let package = Package(
