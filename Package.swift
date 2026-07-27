02b22122e389622aa21f8af2f24b0be750c71be0
Ovi Trif <ovitrif@proton.me>
2026-07-27 16:47:58 +0200

    fix: add android elf build ids

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let tag = "v0.1.0-rc39"
-let checksum = "139f6eff374a71d87ef611485389797c5436cea513077ac2640efad56f50fa89"
+let tag = "v0.1.0-rc40"
+let checksum = "50ed2bc4492baf075e5dec14247dd54722689bfcd7b019f2be57082f42bb22d4"
 let url = "https://github.com/pubky/paykit-rs/releases/download/\(tag)/Paykit.xcframework.zip"
 
 let package = Package(
