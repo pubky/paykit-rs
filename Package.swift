4e2801c441f44a3d85f157716f93a5b7e7045041
Ovi Trif <ovitrif@proton.me>
2026-08-26 19:16:29 +0200

    chore: bump to 0.1.0-rc47

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let tag = "v0.1.0-rc46"
-let checksum = "d0c83a36bacc506d176b91736e21db3c096e76f87db771f755604b49dd518750"
+let tag = "v0.1.0-rc47"
+let checksum = "1d1fc858bd1712ee0cd5965e1839bce502a1209dbd42a09cdb9495f0f8256f6f"
 let url = "https://github.com/pubky/paykit-rs/releases/download/\(tag)/Paykit.xcframework.zip"
 
 let package = Package(
