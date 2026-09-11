3dd8e18ffdedc76fc67a4674c6023adc9ad110f5
benk10 <ben.kaufman10@gmail.com>
2026-09-11 13:27:10 +0300

    Bump Paykit to 0.1.0-rc53

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let tag = "v0.1.0-rc52"
-let checksum = "e005a56472a6cb6b55009fabe5427ad8aaaf5284c52fdb1f4734cb2edf8a14b5"
+let tag = "v0.1.0-rc53"
+let checksum = "63edff3a107faae86e08bd5ea2a66be97116181894aaa207dfcfbac1243af7b4"
 let url = "https://github.com/pubky/paykit-rs/releases/download/\(tag)/Paykit.xcframework.zip"
 
 let package = Package(
